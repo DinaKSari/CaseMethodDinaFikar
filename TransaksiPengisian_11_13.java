1bb7db62bdddb88eb315f5840e79a4c5330189de
DinaKSari <dinakumala7070@gmail.com>
2025-05-27 08:14:14 +0700

    Membuat class transaksi pengisian dan konstruktornya

---
+++ TransaksiPengisian_11_13.java
@@ -0,0 +1,13 @@
+public class TransaksiPengisian_11_13 {
+    Kendaraan_11_13 kendaraan;
+    double liter;
+    double totalBayar;
+
+    public TransaksiPengisian_11_13(Kendaraan_11_13 kendaraan, double liter, double totalBayar) {
+        this.kendaraan = kendaraan;
+        this.liter = liter;
+        this.totalBayar = totalBayar;
+    }
+
+    
+}
