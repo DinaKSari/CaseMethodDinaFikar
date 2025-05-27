36c3636902effd4520e64bc8bfb3397831b56881
FikarBahrul <fikarbahrul99@gmail.com>
2025-05-27 08:11:10 +0700

    penambahan constructor diagram class BBM

---
+++ BBM_13_11.java
@@ -0,0 +1,9 @@
+public class BBM_13_11 {
+    String namaBBM;
+    Double hargaPerLiter;
+
+    public BBM_13_11 (String namaBBM, Double hargaPerLiter) {
+        this.namaBBM = namaBBM;
+        this.hargaPerLiter = hargaPerLiter;
+    }
+}
